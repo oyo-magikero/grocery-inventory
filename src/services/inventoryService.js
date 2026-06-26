6125203b3a3c9bc50052c3a0e9870d8ac232c4ab
oyo-magikero <julmacapugas@gmail.com>
2026-06-26 14:12:36 -0700

    Add delete item functionality

--- src/services/inventoryService.js
+++ src/services/inventoryService.js
@@ -2,22 +2,29 @@ import { db } from "../firebase";
 import {
   collection,
   addDoc,
-  getDocs
+  getDocs,
+  deleteDoc,
+  doc
 } from "firebase/firestore";
 
 const INVENTORY_COLLECTION = "inventory";
 
-// Save item
+// ➕ Add item
 export const addItem = async (item) => {
   await addDoc(collection(db, INVENTORY_COLLECTION), item);
 };
 
-// Get all items
+// 📥 Get all items
 export const getItems = async () => {
   const snapshot = await getDocs(collection(db, INVENTORY_COLLECTION));
 
-  return snapshot.docs.map(doc => ({
+  return snapshot.docs.map((doc) => ({
     id: doc.id,
     ...doc.data()
   }));
+};
+
+// 🗑 Delete item (NEW)
+export const deleteItem = async (id) => {
+  await deleteDoc(doc(db, INVENTORY_COLLECTION, id));
 };
